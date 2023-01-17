20cac27fc65f3ed97d07cc0f39604b847a5b70a5
Francis Vila <fvila@axway.com>
2023-01-17 17:07:22 +0100

    bump

--- src/components/minitoc.test.js
+++ src/components/minitoc.test.js
@@ -1,7 +1,7 @@
 import { render, screen, waitFor, fireEvent } from '@testing-library/react'
 import Minitoc from './minitoc.js'
 
-test('folds content when click on In this Page', async () => {
+test('folds content when click on In this Page ', async () => {
   render(<Minitoc />)
   const inThisPage = screen.getByTitle('click here to fold/unfold the mini table of contents')
   const minitocRoot = screen.getByTitle('minitoc root')
